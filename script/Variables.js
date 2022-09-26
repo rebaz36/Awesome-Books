fc1e5441e72c7e40a861238d0fb1a8df391a79bd
ndorrh <ndorrh@gmail.com>
2022-09-26 20:05:21 +0100

    Update variables.js

--- script/Variables.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable max-classes-per-file */
-const inputTitle = document.getElementById('title');
-const inputAuthor = document.getElementById('author');
-const submitBtn = document.querySelector('.add-btn');
-const bookSection = document.querySelector('.books');
-
-export {
-  inputTitle, inputAuthor, submitBtn, bookSection,
-};
