03e59de5f53da8196c0aa9d2b7616a991b7060af
ndorrh <ndorrh@gmail.com>
2022-09-26 17:38:54 +0100

    Create module for Variables

---
+++ script/Variables.js
@@ -0,0 +1,9 @@
+/* eslint-disable max-classes-per-file */
+const inputTitle = document.getElementById('title');
+const inputAuthor = document.getElementById('author');
+const submitBtn = document.querySelector('.add-btn');
+const bookSection = document.querySelector('.books');
+
+export {
+  inputTitle, inputAuthor, submitBtn, bookSection,
+};
